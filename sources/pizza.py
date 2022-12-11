8e845a9106b4e0409fe9af0eab4d940f96a85efb
Sébastien Thomas-Girard <arieseb@gmail.com>
2022-12-11 13:25:02 +0100

    New pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+    'vesuvio': {
+        'ingredients': {'pepper': 5, 'garlic': 5, 'mushroom': 2},
+        'price': 13,
     }
 }
